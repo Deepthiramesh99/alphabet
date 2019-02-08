62011022a9d39901c062b6e6d9a3a1fdc786950d
Deepthiramesh99 <47448877+Deepthiramesh99@users.noreply.github.com>
2019-02-08 18:37:35 +0530

    Update and rename README.md to alphabets.c

---
+++ alphabets.c
@@ -0,0 +1,13 @@
+int main() {
+    char s;
+    scanf("%c",&s);
+    if(s>='a'||s<='z'||s>='A'||s<='Z')
+    {
+        printf("Alphabet");
+    }
+    else
+    {
+        printf("no");
+    }
+	return 0;
+}
